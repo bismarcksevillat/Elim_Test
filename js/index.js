fb59e9449d9985126d2dc70a08f1ea80121fb1b4
Bismarck Eli Sevilla Torrez <A0ATCBD00003@accedotech.net>
2019-07-24 13:25:11 -0700

    CSS

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   function breakTitle(){
-      $("#future-title").lettering();
+      // $("#future-title").lettering();
+      $("#future-title").lettering('words').children('span').lettering();
   }
 
   breakTitle();
