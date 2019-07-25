e246bfe5c7afe313d5d5adabadd259f99783f564
Bismarck Eli Sevilla Torrez <A0ATCBD00003@accedotech.net>
2019-07-25 09:47:01 -0700

    Timeline

--- js/index.js
+++ js/index.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
   }
 
   breakTitle();
+
+  var tl = new TimelineLite();
+  tl.fromTo(".cellphone::before", 2, {scale:0, opacity:0}, {scale:1, opacity:1});
+  tl.fromTo(".cellphone::after", 2, {scale:0, opacity:0}, {scale:1, opacity:1}, "-=2");
+  tl.fromTo(".cellphone img", 2, {scale:0, opacity:0}, {scale:1, opacity:1});
 });
